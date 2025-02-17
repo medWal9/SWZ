3582c63050969d0f3c5134d7249ed15ba066615b
medWal9 <mz90.walid@hotmail.fr>
2025-02-17 14:25:54 -0500

    testing CI/CD

--- NodeJS-Service/src/routes/products.routes.ts
+++ NodeJS-Service/src/routes/products.routes.ts
@@ -6,6 +6,5 @@ const router = Router();
 // Define the routes for products
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
-//router.post('/', createProduct);
 
 export default router;
